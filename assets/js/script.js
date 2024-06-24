25ef065f0a7007648e18843a90d8fdc90c1d55fc
AmanPunia-510 <puniaaman888@gmail.com>
2024-06-24 09:04:25 +0530

    work on first accordian now

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ accordian_items.forEach((items) => {
   let paragraph = items.querySelector(".accordian-p");
   let heading = items.querySelector(".accordian-txt-or-svg");
   let svg = items.querySelector(".accordian-svg");
+  let main_h = items.querySelector(".accordian-h");
 
   heading.style.cursor = "pointer";
   heading.addEventListener("click", () => {
@@ -12,18 +13,29 @@ accordian_items.forEach((items) => {
       if (other !== items) {
         let paragraph = other.querySelector(".accordian-p");
         let svg = other.querySelector(".accordian-svg");
+        let main_h = other.querySelector(".accordian-h");
+        other.classList.remove("accord_back_clr");
         paragraph.classList.add("d-none");
+        main_h.style.color = "unset";
         svg.innerHTML = "+";
+        svg.style.color = "unset";
       }
     });
     if (paragraph.classList.contains("d-none")) {
+      items.classList.add("accord_back_clr");
       paragraph.classList.remove("d-none");
       paragraph.classList.add("d-block");
+      main_h.style.color = "white";
       svg.innerHTML = "-";
-    } else {
+      svg.style.color = "white";
+    }
+    else {
       paragraph.classList.remove("d-block");
       paragraph.classList.add("d-none");
       svg.innerHTML = "+";
+      svg.style.color = "unset";
+      items.classList.remove("accord_back_clr");
+      main_h.style.color = "unset";
     }
   });
 });
