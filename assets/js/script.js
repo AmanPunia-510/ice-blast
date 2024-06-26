49e41656b50afcd78f8f3cf4ef69fba0088e654c
AmanPunia-510 <puniaaman888@gmail.com>
2024-06-26 12:24:41 +0530

    make all thing in sequnce

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,73 @@
-// ===================================== accrodian ========================================//
+// ===================================== navbar ========================================//
+
+const item = document.querySelector(".navbar-items");
+const menu = document.querySelector(".toggle");
+
+menu.addEventListener("click", () => {
+  item.classList.toggle("show");
+  let contain = document.querySelector("body");
+  let container = document.querySelector(".my-container");
+
+  if (item.classList.contains("show")) {
+    contain.style.position = "fixed";
+    contain.style.top = "0";
+    contain.style.left = "0";
+    contain.style.width = "100%";
+    index.classList.add("z-3");
+    container.classList.add("position-u");
+  } else {
+    contain.style.position = "unset";
+  }
+});
+
+// ===================================== accrodian-1 ========================================//
+
+let accordianItems = document.querySelectorAll(".accordian-box1");
+let layer1 = document.querySelector(".layer-on-p");
+layer1.classList.add("d-block");
+let layers = document.querySelectorAll(".layer-on-p");
+layers.forEach((l) => {
+  l.style.display = "none";
+});
+
+accordianItems.forEach((item) => {
+  let paragraph = item.querySelector(".accordian1-p");
+  let heading = item.querySelector(".acc-p-h2");
+  let svg = item.querySelector(".accordian-svg");
+  let layer = item.querySelector(".layer-on-p");
+
+  heading.style.cursor = "pointer";
+
+  heading.addEventListener("click", () => {
+    accordianItems.forEach((otherItem) => {
+      if (otherItem !== item) {
+        let otherParagraph = otherItem.querySelector(".accordian1-p");
+        let otherSvg = otherItem.querySelector(".accordian-svg");
+        let layer = otherItem.querySelector(".layer-on-p");
+
+        otherParagraph.classList.add("d-none");
+        otherParagraph.classList.remove("d-block");
+        otherSvg.innerHTML = "+";
+        layer.classList.add("d-none");
+      }
+    });
+
+    if (paragraph.classList.contains("d-none")) {
+      paragraph.classList.remove("d-none");
+      paragraph.classList.add("d-block");
+      layer.classList.remove("d-none");
+      layer.classList.add("d-inline-block");
+      svg.innerHTML = "-";
+    } else {
+      paragraph.classList.remove("d-block");
+      paragraph.classList.add("d-none");
+      svg.innerHTML = "+";
+      layer.classList.add("d-none");
+    }
+  });
+});
+
+// ===================================== accrodian-2 ========================================//
 let accordian_items = document.querySelectorAll(".accordian-box");
 
 accordian_items.forEach((items) => {
@@ -40,6 +109,8 @@ accordian_items.forEach((items) => {
   });
 });
 
+// ===================================== SLIDER ========================================//
+
 var gallerySlider = new Swiper(".gallery-slider", {
   pagination: {
     el: ".swiper-pagination",
@@ -67,77 +138,4 @@ var gallerySlider = new Swiper(".gallery-slider", {
       slidesPerView: 2.5,
     },
   },
-});
-
-let accordianItems = document.querySelectorAll(".accordian-box1");
-let layer1 = document.querySelector(".layer-on-p");
-layer1.classList.add("d-block");
-let layers = document.querySelectorAll(".layer-on-p");
-layers.forEach((l) => {
-  l.style.display = "none";
-});
-
-accordianItems.forEach((item) => {
-  let paragraph = item.querySelector(".accordian1-p");
-  let heading = item.querySelector(".acc-p-h2");
-  let svg = item.querySelector(".accordian-svg");
-  let layer = item.querySelector(".layer-on-p");
-
-  // Set the cursor style for the heading
-  heading.style.cursor = "pointer";
-
-  // Add a click event listener to the heading
-  heading.addEventListener("click", () => {
-    // Loop through all accordion items to reset their state
-    accordianItems.forEach((otherItem) => {
-      if (otherItem !== item) {
-        // Select the paragraph and svg of the other items
-        let otherParagraph = otherItem.querySelector(".accordian1-p");
-        let otherSvg = otherItem.querySelector(".accordian-svg");
-        let layer = otherItem.querySelector(".layer-on-p");
-
-        // Hide the paragraph and update the svg for other items
-        otherParagraph.classList.add("d-none");
-        otherParagraph.classList.remove("d-block");
-        otherSvg.innerHTML = "+";
-        layer.classList.add("d-none");
-      }
-    });
-
-    // Toggle the clicked item's paragraph and svg
-    if (paragraph.classList.contains("d-none")) {
-      paragraph.classList.remove("d-none");
-      paragraph.classList.add("d-block");
-      layer.classList.remove("d-none");
-      layer.classList.add("d-inline-block");
-      svg.innerHTML = "-";
-    } else {
-      paragraph.classList.remove("d-block");
-      paragraph.classList.add("d-none");
-      svg.innerHTML = "+";
-      layer.classList.add("d-none");
-    }
-  });
-});
-
-// ===================================== navbar ========================================//
-
-const item = document.querySelector(".navbar-items");
-const menu = document.querySelector(".toggle");
-
-menu.addEventListener("click", () => {
-  item.classList.toggle("show");
-  let contain = document.querySelector("body");
-  let container = document.querySelector(".my-container");
-
-  if (item.classList.contains("show")) {
-    contain.style.position = "fixed";
-    contain.style.top = "0";
-    contain.style.left = "0";
-    contain.style.width = "100%";
-    index.classList.add("z-3");
-    container.classList.add("position-u");
-  } else {
-    contain.style.position = "unset";
-  }
 });
